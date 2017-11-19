e1a03abd258acf5add2eaef0d547c264699a724e
zza167 <zza167@cs-dblab-01.dyn.cs.sfu.ca>
2017-11-18 19:46:51 -0800

    get decentralized matrix

--- Step 2 - User Clustering/matlab/meanAll.m
+++ Step 2 - User Clustering/matlab/meanAll.m
@@ -1,9 +1,19 @@
 rating_matrix = load('../data/sparse_matrix_ml-latest.mat');
 rating_matrix = rating_matrix.content(1:size(rating_matrix.content,1),:);
-[userNum, itemNum] = size(rating_matrix);
-rating_matrix_tenth = rating_matrix(1:int32(userNum*0.1),find(sum(rating_matrix)));
+[userNum, ~] = size(rating_matrix);
+decentralized_matrix = rating_matrix(1:int32(userNum*0.1),find(sum(rating_matrix)));
+[~, itemNum] = size(decentralized_matrix);
+clear rating_matrix
 
-mean_all = sum(rating_matrix_tenth, 2) ./ sum(rating_matrix_tenth~=0, 2);
+mean_all = sum(decentralized_matrix, 2) ./ sum(decentralized_matrix~=0, 2);
 mean_all = single(full(mean_all));
-disp('done');
 save('../data/mean_all.mat', 'mean_all');
+disp('mean_all DONE');
+
+decentralized_matrix = single(full(decentralized_matrix));
+disp('decentralized_matrix 1 DONE');
+
+decentralized_matrix = sparse(double(decentralized_matrix - (decentralized_matrix~=0) .* repmat(mean_all, 1, itemNum)));
+disp('decentralized_matrix 2 DONE');
+save('../data/decentralized_matrix.mat', 'decentralized_matrix');
+disp('save DONE');
