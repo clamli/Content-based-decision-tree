a657d827f301f432283377fe52d8d7589031d7ba
zzhyzzh <zzhno50@hotmail.com>
2017-11-24 13:22:26 -0800

    run big mf

--- Step 5 - Prediction Model/matlab/cost_func.m
+++ Step 5 - Prediction Model/matlab/cost_func.m
@@ -5,14 +5,21 @@ function [J,grad] = cost_func(obj, Y, R, rank, lambda )
     [item_num, user_num] = size(R);
     item_vectors = reshape(obj(1:item_num * rank),       item_num, rank);
     user_vectors = reshape(obj(item_num * rank + 1:end), user_num, rank);
-            
-    J = 1/2 * sum(sum((R .* (item_vectors * user_vectors'-Y)).^2))...
+%     disp('reshape DONE')
+    
+    temp = R .* (item_vectors * user_vectors' - Y);
+%     disp('temp DONE')
+    
+    J = 1/2 * sum(sum(temp.^2))...
             + lambda/2 * sum(sum(user_vectors.^2)) ...
             + lambda/2 * sum(sum(item_vectors.^2));
-
-    item_grad =  R .* (item_vectors * user_vectors' - Y)   * user_vectors + lambda * item_vectors;
-    user_grad = (R .* (item_vectors * user_vectors' - Y))' * item_vectors + lambda * user_vectors;
-
+%     disp('J DONE')
+    
+    item_grad =  temp   * user_vectors + lambda * item_vectors;
+    user_grad =  temp'  * item_vectors + lambda * user_vectors;
+%     disp('Grad DONE')
+    
     grad = [item_grad(:); user_grad(:)];
+%     disp('cost function DONE')
 end
 
