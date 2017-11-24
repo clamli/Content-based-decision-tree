7184fe289f6ed3d6641def8e6790b05c7c62c63a
clamli <boyizjuer2017@gmail.com>
2017-11-24 01:10:02 -0800

    solve divide 0 problem

--- Step 3 - Item Similarity Information/cosine_similarity.m
+++ Step 3 - Item Similarity Information/cosine_similarity.m
@@ -1,6 +1,6 @@
 function [ cosine_similarity ] = cosine_similarity( content )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    cosine_similarity = (content * content') ./ (sqrt(sum(content.^2, 2)) * sqrt(sum((content').^2, 1)));
+    cosine_similarity = (content * content') ./ (sqrt(sum(content.^2, 2)) * sqrt(sum((content').^2, 1)) + 1e-9);
 end
 
