2b2589d12033b2e55bbc308c2a13e62aea5c6eaf
clamli <boyizjuer2017@gmail.com>
2017-11-29 14:12:07 -0800

    change node interval

--- Step 4 - Construct the Decision Tree/ContentDecisionTree.m
+++ Step 4 - Construct the Decision Tree/ContentDecisionTree.m
@@ -28,6 +28,7 @@ classdef ContentDecisionTree<handle
                               % }
         tree;                 % Array of movie index  [....]
         tree_bound;           % Node bound for each level of the tree, Cell
+        interval_bound;       % Bound for each node's interval
         user_cluster_id;      % Id of user clusters in the cell
     end
     
@@ -146,18 +147,23 @@ classdef ContentDecisionTree<handle
             min_error = -1;
             for i = 1:num_candidate_cluster
                 item_average_rating = mean(rating_for_item_in_node(index_cell{i}, :), 1);
-                dislike_array = tmp_UI_matrix_in_node(:, item_average_rating <= 3.61);
-                mediocre_array = tmp_UI_matrix_in_node(:, item_average_rating > 3.61 & item_average_rating <= 3.63);
-                like_array = tmp_UI_matrix_in_node(:, item_average_rating > 3.63);
+                [afts, ind] = sort(item_average_rating);
+                interval1 = item_average_rating(ind(round(size(ind, 2)/3)));
+                interval2 = item_average_rating(ind(round(2*size(ind, 2)/3))); 
+                dislike_array = tmp_UI_matrix_in_node(:, item_average_rating <= interval1);
+                mediocre_array = tmp_UI_matrix_in_node(:, item_average_rating > interval1 & item_average_rating <= interval2);
+                like_array = tmp_UI_matrix_in_node(:, item_average_rating > interval2);
                 error = sum(sum(dislike_array.^2, 2)-(sum(dislike_array, 2).^2)./(sum(dislike_array~=0, 2)+1e-9)) + ...
                     sum(sum(mediocre_array.^2, 2)-(sum(mediocre_array, 2).^2)./(sum(mediocre_array~=0, 2)+1e-9)) + ...
                     sum(sum(like_array.^2, 2)-(sum(like_array, 2).^2)./(sum(like_array~=0, 2)+1e-9));
-                fprintf('cluster id: %d\n', i);
-                fprintf('index_cell: %d\n', size(index_cell{i}));
-                fprintf('%.2f        %.2f\n', error, min_error);
+%                 fprintf('cluster id: %d\n', i);
+%                 fprintf('index_cell: %d\n', size(index_cell{i}));
+%                 fprintf('%.2f        %.2f\n', error, min_error);
                 fprintf('%d, %d, %d\n', size(dislike_array, 2), size(mediocre_array, 2), size(like_array, 2));
                 if min_error == -1 || error < min_error
                     min_error = error;
+                    min_interval1 = interval1;
+                    min_interval2 = interval2;
                     min_usr_cluster_id = i;
                     min_item_average_rating = item_average_rating;
                 end
@@ -171,9 +177,9 @@ classdef ContentDecisionTree<handle
             %% Assign Children Node
             %disp(min_item_average_rating);
             %disp(size(item_in_node));
-            dislike_array = item_in_node(min_item_average_rating <= 3.61);
-            mediocre_array = item_in_node(min_item_average_rating > 3.61 & min_item_average_rating <= 3.63);
-            like_array = item_in_node(min_item_average_rating >= 3.63);
+            dislike_array = item_in_node(min_item_average_rating <= min_interval1);
+            mediocre_array = item_in_node(min_item_average_rating > min_interval1 & min_item_average_rating <= min_interval2);
+            like_array = item_in_node(min_item_average_rating >= min_interval2);
 %             disp(size(obj.tree(tree_bound_for_node(1):tree_bound_for_node(2))));
 %             disp(size(dislike_array));
 %             disp(size(mediocre_array));
@@ -187,11 +193,13 @@ classdef ContentDecisionTree<handle
                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound1+1, bound2]];
                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound2+1, bound3]];
                 obj.split_cluster{obj.cur_depth-1} = candidate_user_cluster_id(min_usr_cluster_id);
+                obj.interval_bound{obj.cur_depth-1} = {[min_interval1, min_interval2]};
             else
                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [tree_bound_for_node(1), bound1]];
                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound1+1, bound2]];
                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound2+1, bound3]];
                 obj.split_cluster{obj.cur_depth-1} = [obj.split_cluster{obj.cur_depth-1}, candidate_user_cluster_id(min_usr_cluster_id)];
+                obj.interval_bound{obj.cur_depth-1} = [obj.interval_bound{obj.cur_depth-1}, [min_interval1, min_interval2]];
             end
             %fprintf('    Assign Child Nodes finished!\n');
             
@@ -208,73 +216,6 @@ classdef ContentDecisionTree<handle
             
             obj.cur_node = obj.cur_node + 3;
             fprintf('Current depth: %d        %.2f%%\n', obj.cur_depth, 100*obj.cur_node/obj.node_num);
-%             for i = 1:num_candidate_cluster
-%                 fprintf('level %d, cluster %d\n', obj.cur_depth ,i);
-%                 
-%                 userid_in_cluster = obj.user_cluster{candidate_user_cluster_id(i)};
-%                 %% Calculate Score
-%                 % Get user rating matrix for this node
-%                 user_rating_matrix = single(full(...
-%                                         obj.UI_matrix(...
-%                                             userid_in_cluster, :)));
-% 
-%                 % Generated Rating Matrix
-%                 generated_rating_matrix = (user_rating_matrix(:, item_in_node) == 0) .* (user_rating_matrix*obj.item_sim_matrix(:, item_in_node));
-%                 generated_rating_matrix = (generated_rating_matrix' / diag(sum(user_rating_matrix ~= 0, 2)))';
-%                          
-%                 % Whole Rating Matrix
-%                 rating_for_item_in_node = user_rating_matrix(:, item_in_node) + generated_rating_matrix;
-%                 clear generated_rating_matrix;
-%                 clear user_rating_matrix;
-%                 
-%                 fprintf('    Calculate score finished!');
-%                 
-%                 %% Calculate Error
-%                 item_average_rating = mean(rating_for_item_in_node, 1);     
-%                 dislike_array = rating_for_item_in_node(:, item_average_rating < 2.5);
-%                 mediocre_array = rating_for_item_in_node(:, item_average_rating >= 2.5 & item_average_rating <= 3.5);
-%                 like_array = rating_for_item_in_node(:, item_average_rating > 3.5);
-%                 clear rating_for_item_in_node;
-%                 error = sum(sum(dislike_array.^2, 2)-(sum(dislike_array, 2).^2)/size(dislike_array, 2)) + ...
-%                     sum(sum(mediocre_array.^2, 2)-(sum(mediocre_array, 2).^2)/size(mediocre_array, 2)) + ...
-%                     sum(sum(like_array.^2, 2)-(sum(like_array, 2).^2)/size(like_array, 2));
-%                 if min_error == -1 || error < min_error
-%                     min_error = error;
-%                     min_usr_cluster_id = i;
-%                     min_item_average_rating = item_average_rating;
-%                 end
-%                 fprintf('    Calculate error finished!');
-%             end
-%             clear item_average_rating;
-%             
-%             %% Assign Children Node
-%             dislike_array = item_in_node(min_item_average_rating < 2.5);
-%             mediocre_array = item_in_node(min_item_average_rating >= 2.5 & min_item_average_rating <= 3.5);
-%             like_array = item_in_node(min_item_average_rating > 3.5);
-%             obj.tree(tree_bound_for_node(1):tree_bound_for_node(2)) = [dislike_array, mediocre_array, like_array];
-%             bound1 = tree_bound_for_node(1)+size(dislike_array, 2)-1;
-%             bound2 = bound1 + size(mediocre_array, 2);
-%             bound3 = bound2 + size(like_array, 2);
-%             if size(obj.tree_bound, 2) < obj.cur_depth
-%                 obj.tree_bound{obj.cur_depth} = [tree_bound_for_node(1), bound1];
-%                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound1+1, bound2]];
-%                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound2+1, bound3]];
-%                 obj.split_cluster{obj.cur_depth} = candidate_user_cluster_id(min_usr_cluster_id);
-%             else
-%                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [tree_bound_for_node(1), bound1]];
-%                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound1+1, bound2]];
-%                 obj.tree_bound{obj.cur_depth} = [obj.tree_bound{obj.cur_depth}, [bound2+1, bound3]];
-%                 obj.split_cluster{obj.cur_depth} = [obj.split_cluster{obj.cur_depth}, candidate_user_cluster_id(min_usr_cluster_id)];
-%             end
-%             
-%             %% Child Nodes Recursion
-%             candidate_user_cluster_id(min_usr_cluster_id) = [];
-%             obj.generateDecisionTree(obj.tree_bound{obj.cur_depth}(end-2), candidate_user_cluster_id);    % dislike node
-%             obj.cur_depth = obj.cur_depth - 1;
-%             obj.generateDecisionTree(obj.tree_bound{obj.cur_depth}(end-1), candidate_user_cluster_id);    % mediocre node
-%             obj.cur_depth = obj.cur_depth - 1;
-%             obj.generateDecisionTree(obj.tree_bound{obj.cur_depth}(end), candidate_user_cluster_id);      % like node
-%             obj.cur_depth = obj.cur_depth - 1;
         end
         
         function buildTree(obj)
