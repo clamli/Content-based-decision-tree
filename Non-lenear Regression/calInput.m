deb34100b2b3e1f2d9f20d9d758e4247cd0c91db
zzhyzzh <zzhno50@hotmail.com>
2017-11-30 11:04:29 -0800

    correct spelling

--- Non-lenear Regression/calInput.m
+++
@@ -1,9 +0,0 @@
-function [ result ] = calInput( UI_Matrix, sim_matrix, train, test )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    rs = (UI_Matrix(:, train)*sim_matrix(train, test)) .* (UI_Matrix(:, test)~=0);
-    result = rs(UI_Matrix(:, test)~=0);
-
-end
-
--- Non-lenear Regression/callInput_denominator.m
+++
@@ -1,9 +0,0 @@
-function [ result ] = callInput_denominator( UI_Matrix, sim_matrix, train, test )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-    rs = ((UI_Matrix(:, train)~=0)*sim_matrix(train, test)) .* (UI_Matrix(:, test)~=0);
-    result = rs(UI_Matrix(:, test)~=0);
-
-end
-
