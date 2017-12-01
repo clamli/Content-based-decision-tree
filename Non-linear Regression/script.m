143f20345dbb0d21f5e198f47c0014d77802489f
zzhyzzh <zzhno50@hotmail.com>
2017-12-01 14:00:16 -0800

    modify regresson code

--- Non-linear Regression/script.m
+++ Non-linear Regression/script.m
@@ -1,54 +1,58 @@
-item_id_random = randperm(44480);
-
-train = item_id_random(1:round(44480 * 0.7));
-save('data/train.mat', 'train', '-v7.3');
-test = item_id_random(round(44480 * 0.7) + 1:end);
-save('data/test.mat', 'test', '-v7.3');
-
-load('../Step 2 - User Clustering/data/sparse_matrix_ml-latest.mat')
-startPos = 1;
-endPos = round(270896*0.1);
-UI_matrix = single(full(UI_matrix(startPos : endPos, :)));
+name = '1m';
+load(['../Step 2 - User Clustering/data/sparse_matrix_ml-',...
+        name, '.mat'])
+[~, itemNum] = size(UI_matrix);
+% For large dataset, pick 10% users here.
+% For 1m dataset, pick all the users
+UI_matrix = single(full(UI_matrix));
 % Eliminate users who don't have rating within train items.
 UI_matrix = UI_matrix(sum(UI_matrix(:, train),2)~=0, :);
+
+item_id_random = randperm(itemNum);
+
+train = item_id_random(1:round(itemNum * 0.7));
+save(['data/', name, '/train.mat'], 'train', '-v7.3');
+test = item_id_random(round(itemNum * 0.7) + 1:end);
+save(['data/', name, '/test.mat'], 'test', '-v7.3');
+
 %% title
-load('data/title_matrix_44480_44480.mat')
+% load('data/title_matrix_44480_44480.mat')
 rs = calInput_nominator( UI_matrix, title_matrix, train, test );
-save('data/title.mat', 'rs', '-v7.3');
+save(['data/', name, '/title.mat'], 'rs', '-v7.3');
 rs = calInput_denominator( UI_matrix, title_matrix, train, test );
-save('data/title_den.mat', 'rs', '-v7.3');
+save(['data/', name, '/title_den.mat'], 'rs', '-v7.3');
 clear title_matrix
 disp('title done!')
 
 %% tag
-load('data/tag_matrix_44480_44480.mat')
+% load('data/tag_matrix_44480_44480.mat')
 rs = calInput_nominator( UI_matrix, tag_matrix, train, test );
-save('data/tag.mat', 'rs', '-v7.3');
+save(['data/', name, '/tag.mat'], 'rs', '-v7.3');
 rs = calInput_denominator( UI_matrix, tag_matrix, train, test );
-save('data/tag_den.mat', 'rs', '-v7.3');
+save(['data/', name, '/tag_den.mat'], 'rs', '-v7.3');
 clear tag_matrix
 disp('tag done!')
 
 %% year
-load('data/year_matrix_44480_44480.mat')
+% load('data/year_matrix_44480_44480.mat')
 rs = calInput_nominator( UI_matrix, year_matrix, train, test );
-save('data/year.mat', 'rs', '-v7.3');
+save(['data/', name, '/year.mat'], 'rs', '-v7.3');
 rs = calInput_denominator( UI_matrix, year_matrix, train, test );
-save('data/year_den.mat', 'rs', '-v7.3');
+save(['data/', name, '/year_den.mat'], 'rs', '-v7.3');
 clear year_matrix
 disp('year done!')
 
 %% genre
-load('data/genre_matrix_44480_44480.mat')
+% load('data/genre_matrix_44480_44480.mat')
 rs = calInput_nominator( UI_matrix, genre_matrix, train, test );
-save('data/genre.mat', 'rs', '-v7.3');
+save(['data/', name, '/genre.mat'], 'rs', '-v7.3');
 rs = calInput_denominator( UI_matrix, genre_matrix, train, test );
-save('data/genre_den.mat', 'rs', '-v7.3');
+save(['data/', name, '/genre_den.mat'], 'rs', '-v7.3');
 clear genre_matrix
 disp('genre done!')
 
 %% real rating
 UI_matrix_test = UI_matrix(:, test);
 real_rating = UI_matrix_test(UI_matrix_test~=0);
-save('data/real_rating.mat', 'real_rating', '-v7.3');
+save(['data/', name, '/real_rating.mat'], 'real_rating', '-v7.3');
 disp('real rating done!')
--- Non-linear Regression/trainModel.m
+++ Non-linear Regression/trainModel.m
@@ -1,24 +1,26 @@
-A_title = load('data/title.mat');
+name = '1m';
+
+A_title = load(['data/', name, '/title.mat']);
 A_title = A_title.rs;
-B_title = load('data/title_den.mat');
+B_title = load(['data/', name, '/title_den.mat']);
 B_title = B_title.rs;
 
-A_tag = load('data/tag.mat');
+A_tag = load(['data/', name, '/tag.mat']);
 A_tag = A_tag.rs;
-B_tag = load('data/tag_den.mat');
+B_tag = load(['data/', name, '/tag_den.mat']);
 B_tag = B_tag.rs;
 
-A_year = load('data/year.mat');
+A_year = load(['data/', name, '/year.mat']);
 A_year = A_year.rs;
-B_year = load('data/year_den.mat');
+B_year = load(['data/', name, '/year_den.mat']);
 B_year = B_year.rs;
 
-A_genre = load('data/genre.mat');
+A_genre = load(['data/', name, '/genre.mat']);
 A_genre = A_genre.rs;
-B_genre = load('data/genre_den.mat');
+B_genre = load(['data/', name, '/genre_den.mat']);
 B_genre = B_genre.rs;
 
-realRatings  = load('data/real_rating.mat');
+realRatings  = load(['data/', name, '/real_rating.mat']);
 realRatings = realRatings.real_rating;
 tbl = table(A_title, A_tag, A_year, A_genre,...
             B_title, B_tag, B_year, B_genre, realRatings);
@@ -31,8 +33,11 @@ mdl = fitnlm(tbl,modelfun,beta0)
 %% Test with specific params
 x = [A_title, A_tag, A_year, A_genre,...
             B_title, B_tag, B_year, B_genre, realRatings];
-b = [32.726 0.33067 0.50134 0.44417];
-modelfun = (b(1).*x(:,1) + b(2).*x(:,2) +  b(3).*x(:,3) +  b(4).*x(:,4))...
+params = table2array(mdl.Coefficients(:,1));
+save(['data/', name, '/params.mat'], 'params');
+modelfun = (params(1).*x(:,1) + params(2).*x(:,2) +  params(3).*x(:,3) +  params(4).*x(:,4))...
                                      ./...
-                 (b(1).*x(:,5) + b(2).*x(:,6) +  b(3).*x(:,7) +  b(4).*x(:,8));
+                 (params(1).*x(:,5) + params(2).*x(:,6) +  params(3).*x(:,7) +  params(4).*x(:,8));
 rmse = (sum((realRatings - modelfun).^2)/length(realRatings)).^0.5
+
+%% Generate resulting 
